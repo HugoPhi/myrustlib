512be4638b956b137bb2e7b1294f3ddf5fa6f4f2
HugoPhi <hugonelson07@outlook.com>
2023-08-08 17:11:31 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use stdstruct::stack::Stack;
+use utils;
+
+fn main() {
+    // test for SourceLinkList 
+    // let mut list = SourceLinkList::new();
+    // list.push(1);
+    // list.push(2);
+    // list.push(3);
+    // list.push(4);
+    // list.push(5);
+    // list.push(6);
+    // println!("{:?}", list);
+    // while list.pop() {
+    //     println!("{:?}", list);
+    // }
+    
+
+    // test for stack 
+    let mut stack: Stack<i32> = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.push(6);
+    println!("{:?}", stack);
+    while !stack.empty() {
+        println!("{:?}", stack);
+        stack.pop();
+    }
+    println!("{:?}", stack);
+}
+
